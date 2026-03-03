2b0376cd2ad7aaa3b3b7b78e3c1442fb8e90e2b1
fahadd002 <fpramanik6@gmail.com>
2026-03-03 22:46:39 +0600

    Create all database

--- src/generated/prisma/enums.ts
+++ src/generated/prisma/enums.ts
@@ -10,10 +10,10 @@
 */
 
 export const Role = {
-  PATIENT: 'PATIENT',
-  DOCTOR: 'DOCTOR',
+  SUPER_ADMIN: 'SUPER_ADMIN',
   ADMIN: 'ADMIN',
-  SUPER_ADMIN: 'SUPER_ADMIN'
+  DOCTOR: 'DOCTOR',
+  PATIENT: 'PATIENT'
 } as const
 
 export type Role = (typeof Role)[keyof typeof Role]
@@ -22,7 +22,6 @@ export type Role = (typeof Role)[keyof typeof Role]
 export const UserStatus = {
   ACTIVE: 'ACTIVE',
   BLOCKED: 'BLOCKED',
-  SUSPENDED: 'SUSPENDED',
   DELETED: 'DELETED'
 } as const
 
@@ -31,7 +30,40 @@ export type UserStatus = (typeof UserStatus)[keyof typeof UserStatus]
 
 export const Gender = {
   MALE: 'MALE',
-  FEMALE: 'FEMALE'
+  FEMALE: 'FEMALE',
+  OTHER: 'OTHER'
 } as const
 
 export type Gender = (typeof Gender)[keyof typeof Gender]
+
+
+export const BloodGroup = {
+  A_POSITIVE: 'A_POSITIVE',
+  A_NEGATIVE: 'A_NEGATIVE',
+  B_POSITIVE: 'B_POSITIVE',
+  B_NEGATIVE: 'B_NEGATIVE',
+  AB_POSITIVE: 'AB_POSITIVE',
+  AB_NEGATIVE: 'AB_NEGATIVE',
+  O_POSITIVE: 'O_POSITIVE',
+  O_NEGATIVE: 'O_NEGATIVE'
+} as const
+
+export type BloodGroup = (typeof BloodGroup)[keyof typeof BloodGroup]
+
+
+export const AppointmentStatus = {
+  SCHEDULED: 'SCHEDULED',
+  INPROGRESS: 'INPROGRESS',
+  COMPLETED: 'COMPLETED',
+  CANCELED: 'CANCELED'
+} as const
+
+export type AppointmentStatus = (typeof AppointmentStatus)[keyof typeof AppointmentStatus]
+
+
+export const PaymentStatus = {
+  PAID: 'PAID',
+  UNPAID: 'UNPAID'
+} as const
+
+export type PaymentStatus = (typeof PaymentStatus)[keyof typeof PaymentStatus]
